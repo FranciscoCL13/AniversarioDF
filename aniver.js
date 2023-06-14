8cb1602d2c9af0f0cbc9814a26eb61366d6ac7d2
FranciscoCL <98781874+FranciscoCL13@users.noreply.github.com>
2023-06-13 21:12:42 -0600

    aniversario

---
+++ aniver.js
@@ -0,0 +1,57 @@
+// Función para mostrar mensajes de amor
+function mostrar() {
+  var mensajes = [
+    "4.8 años como papás.  Vástago: Daník Contreras Moreno <3 ⭐",
+    "6 años viviendo juntos.   Lugar: México CDMX 👨‍👩‍👧",
+    "12 años festejamos hoy!.   Razón: Felíz Aniversario D&F P.T.L.V. & P.S. 🥳❤️",
+    "Y 14 en abonos jaja.   Abonos: 2 años brutos 'Adolescentes 16-18 años'👶 ",
+    "20 años de conocernos.   Origen: Christel House 🐣",
+    "Toda una vida por hacer equipo, imaginar y materializar.   Destino: Todos los rincones del mundo 🌎🌍🌏",
+    
+  ];
+
+  // Obtener un mensaje
+  // var mensaje = mensajes[Math.floor(Math.random() * mensajes.length)];
+  
+mensajes.forEach(function(mensaje){
+  return window.alert(mensaje);
+});
+  // log.console("Mensaje ", mensaje);
+  }
+
+  
+//---------------------------
+
+
+  // Función para mostrar mensajes de amor
+function mostrardos() {
+  var mensajes = [
+    "Eres el amor de mi vida 💝 👶",
+    "U´re so hot!!! 😍😈 ",
+    "Mi corazón late más fuerte cuando estoy contigo 🌹💑",
+    "Eres una persona increíblemente talentosa 🤓",
+    "Te amo más de lo que las palabras pueden expresar ❤️❤️❤️",
+    "Eres una persona admirable en todos los sentidos. 🤑",
+    "Me gustas todo ❤.❤",
+    "Admiro tu fortaleza y determinación. 💪",
+  ];
+
+  // Obtener un mensaje
+  // var mensaje = mensajes[Math.floor(Math.random() * mensajes.length)];
+  
+mensajes.forEach(function(mensaje){
+  return window.alert(mensaje);
+});
+  // log.console("Mensaje ", mensaje);
+  }
+
+  function mostrartres() {
+    var mensajes = " ";
+  
+    // Obtener un mensaje
+    // var mensaje = mensajes[Math.floor(Math.random() * mensajes.length)];
+    
+  return window.alert(mensajes)
+    // log.console("Mensaje ", mensaje);
+    }
+  
